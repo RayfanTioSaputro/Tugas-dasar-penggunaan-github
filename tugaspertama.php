74aa205e51e627f0fc34d5c6d3b9949fe664db3c
RayfanTioSaputro <57783656+RayfanTioSaputro@users.noreply.github.com>
2020-04-07 21:59:17 -1200

    Menambahkan No. Hp dan Email dibaris berbeda kemudian menghapus baris alamat rumah.

--- tugaspertama.php
+++ tugaspertama.php
@@ -1,6 +1,7 @@
 <?php
      echo "Nama    : Rayfan Tio Saputro <br>";
-    echo "Kelas   : XI RPL 2 <br>";
-    echo "Sekolah : SMK Taruna Bhakti Depok <br>";
-    echo "Alamat  : Jl. H. Dimun, Gg. Damai 2, RT 02/022, No.18, Depok <br>";
+     echo "Kelas   : XI RPL 2 <br>";
+     echo "Sekolah : SMK Taruna Bhakti Depok <br>";
+     echo "No. HP  : 081284791290 <br>";
+     echo "E-mail  : tiosaputrorayfan@gmail.com";
 ?>
