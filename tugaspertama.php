80f23dfd34ca96ae049f62336a0fc1133bf36dbe
RayfanTioSaputro <57783656+RayfanTioSaputro@users.noreply.github.com>
2020-04-07 21:55:15 -1200

    Menambahkan file php

---
+++ tugaspertama.php
@@ -0,0 +1,6 @@
+<?php
+     echo "Nama    : Rayfan Tio Saputro <br>";
+    echo "Kelas   : XI RPL 2 <br>";
+    echo "Sekolah : SMK Taruna Bhakti Depok <br>";
+    echo "Alamat  : Jl. H. Dimun, Gg. Damai 2, RT 02/022, No.18, Depok <br>";
+?>
